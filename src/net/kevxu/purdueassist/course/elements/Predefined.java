99b7c421d37758596f05f2a548adbea41df425f3
Kaiwen Xu <xu227@purdue.edu>
2013-02-16 03:03:42 -0500

    add Type.
    add Type Clinic3.

--- src/net/kevxu/purdueassist/course/elements/Predefined.java
+++ src/net/kevxu/purdueassist/course/elements/Predefined.java
@@ -122,7 +122,7 @@ public class Predefined {
 				"Clinic", ""), Lecture1("Lecture1", ""), Presentation(
 				"Presentation", ""), TravelTime("TravelTime", ""), Experiential1(
 				"Experiential1", ""), Clinic1("Clinic1", ""), Clinic2(
-				"Clinic2", "");
+				"Clinic2", ""), Clinic3("Clinic3", "");
 
 		private final String name;
 		private final String linkName;
