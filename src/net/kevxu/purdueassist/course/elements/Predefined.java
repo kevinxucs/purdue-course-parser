c545eabcf6bee8c8d5a9360ccdc98600cf922b19
Kaiwen Xu <xu227@purdue.edu>
2013-02-16 02:06:33 -0500

    add Type.
    add Type TravelTime.

--- src/net/kevxu/purdueassist/course/elements/Predefined.java
+++ src/net/kevxu/purdueassist/course/elements/Predefined.java
@@ -119,7 +119,7 @@ public class Predefined {
 				"Laboratory Preparation", ""), Experiential("Experiential", ""), Research(
 				"Research", ""), Studio("Studio", ""), Lab1("Lab1", ""), Clinic(
 				"Clinic", ""), Lecture1("Lecture1", ""), Presentatio(
-				"Presentation", "");
+				"Presentation", ""), TravelTime("TravelTime", "");
 
 		private final String name;
 		private final String linkName;
