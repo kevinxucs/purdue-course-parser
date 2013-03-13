e4c1a9d4c2d0e5f7ca642565ab5f77f492e1f745
Kaiwen Xu <kevin@kevxu.net>
2013-03-12 22:41:24 -0400

    Building SearchConfig.

--- src/main/java/net/kevxu/purdueassist/course/elements/SearchConfig.java
+++ src/main/java/net/kevxu/purdueassist/course/elements/SearchConfig.java
@@ -1,14 +1,20 @@
 package net.kevxu.purdueassist.course.elements;
 
+import net.kevxu.purdueassist.course.elements.Predefined.Level;
 import net.kevxu.purdueassist.course.elements.Predefined.Subject;
 import net.kevxu.purdueassist.course.elements.Predefined.Term;
+import net.kevxu.purdueassist.course.elements.Predefined.Type;
 
 public class SearchConfig {
 
 	private Term term;
 	private Subject subject;
 	private int cnbr;
-	private int[] cnbr_range;
+	private int[] cnbrRange;
 	private String title;
+	private Level level;
+	private Type scheduleType;
+	private String college;
+	
 
 }
