bdd31c2329803141f4f81df5ac6c1e27920eb23c
Kaiwen Xu <kevin@kevxu.net>
2013-03-13 18:51:54 -0400

    Removed unused import statements.

--- src/main/java/net/kevxu/purdueassist/course/ScheduleDetail.java
+++ src/main/java/net/kevxu/purdueassist/course/ScheduleDetail.java
@@ -30,7 +30,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import net.kevxu.purdueassist.course.elements.Predefined.Level;
--- src/test/java/net/kevxu/purdueassist/test/ScheduleDetailTest.java
+++ src/test/java/net/kevxu/purdueassist/test/ScheduleDetailTest.java
@@ -27,11 +27,9 @@
 package net.kevxu.purdueassist.test;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
